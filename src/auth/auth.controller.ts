bb23fefedf4c690b1a57ef28b6a8f59a1183cd70
aysar987 <aysrakbar2006@gmail.com>
2026-01-05 23:06:37 +0800

    BE structure guard

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,17 +1,27 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, Get, UseGuards, Req } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('auth')
 export class AuthController {
   constructor(private auth: AuthService) {}
 
+  // PUBLIC
   @Post('register')
   register(@Body() body: any) {
     return this.auth.register(body.email, body.password, body.name);
   }
 
+  // PUBLIC
   @Post('login')
   login(@Body() body: any) {
     return this.auth.login(body.email, body.password);
+  }
+
+  // 🔒 PROTECTED (JWT REQUIRED)
+  @UseGuards(AuthGuard('jwt'))
+  @Get('me')
+  me(@Req() req: any) {
+    return req.user;
   }
 }
